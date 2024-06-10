697d150e085b5102b66b49b7fe070a3cfa38b1ad
Asim Manzoor <asimmanzoor786786@gmail.com>
2024-06-11 01:23:53 +0530

    adding lib code for UtilService

---
+++ src/main/java/com/library/utils/services/UtilService.java
@@ -0,0 +1,11 @@
+package com.library.utils.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtilService {
+
+    public String getString() {
+        return "Hello from Util Service !";
+    }
+}
---
+++ src/test/java/com/library/utils/LibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class LibsApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
